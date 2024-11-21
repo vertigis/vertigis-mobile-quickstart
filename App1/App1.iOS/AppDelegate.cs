57cdb19850746da828bc83f33d664163af41391f
Felicity Rhone <frhone@latitudegeo.com>
2024-11-21 11:39:19 -0800

    Run upgrade tool

--- App1/App1.iOS/AppDelegate.cs
+++ App1/App1.iOS/AppDelegate.cs
@@ -1,53 +1,14 @@
-﻿using Foundation;
-using UIKit;
-using VertiGIS.Mobile;
-using VertiGIS.Mobile.Platform;
-using Xamarin.Forms;
+using Foundation;
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
 
-namespace App1.iOS
+namespace App1.iOS;
+
+// TODO: Platform specific bootstrapping code should be migrated from AppDelegate.cs.original to AppDelegate.cs or MauiProgram.cs.
+// See iOS App Lifecycle: https://learn.microsoft.com/dotnet/maui/fundamentals/app-lifecycle#ios
+
+[Register(nameof(AppDelegate))]
+public class AppDelegate : MauiUIApplicationDelegate
 {
-    // The UIApplicationDelegate for the application. This class is responsible for launching the 
-    // User Interface of the application, as well as listening (and optionally responding) to 
-    // application events from iOS.
-    [Register("AppDelegate")]
-    public partial class AppDelegate : VertiGISAppDelegate
-    {
-        private App _viewerApp;
-
-        //
-        // This method is invoked when the application has loaded and is ready to run. In this 
-        // method you should instantiate the window, load the UI into it and then make the window
-        // visible.
-        //
-        // You have 17 seconds to return from this method, or iOS will terminate your application.
-        //
-        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
-        {
-            HandleExceptions();
-            IOSInitializer.Init();
-            Forms.Init();
-            var viewerApp = new App();
-            _viewerApp = viewerApp;
-
-            // Handle startup urls
-            // See below comment in OpenUrl, then uncomment the code below
-            // SetLaunchUrl(options);
-
-            LoadApplication(viewerApp);
-            return base.FinishedLaunching(app, options);
-        }
-
-        public override bool OpenUrl(UIApplication app, NSUrl url, NSDictionary options)
-        {
-            // Handle startup urls
-            // Add a URL type to Info.plist, then uncomment the code below
-            // return HandleOpenUrl(url);
-            return false;
-        }
-
-        override public UIInterfaceOrientationMask GetSupportedInterfaceOrientations(UIApplication application, UIWindow forWindow)
-        {
-            return GetSupportedOrientations();
-        }
-    }
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
