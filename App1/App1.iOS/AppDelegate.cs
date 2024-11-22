6d597ca63fe636c370509e8fe03252a7a7f324af
Felicity Rhone <frhone@latitudegeo.com>
2024-11-21 17:23:51 -0800

    AppDelegate subclass

--- App1/App1.iOS/AppDelegate.cs
+++ App1/App1.iOS/AppDelegate.cs
@@ -1,6 +1,11 @@
 using Foundation;
 using Microsoft.Maui;
 using Microsoft.Maui.Hosting;
+using System.Diagnostics;
+using System;
+using UIKit;
+using VertiGIS.Mobile;
+using VertiGIS.Mobile.Platforms;
 
 namespace App1.iOS;
 
@@ -8,7 +13,7 @@ namespace App1.iOS;
 // See iOS App Lifecycle: https://learn.microsoft.com/dotnet/maui/fundamentals/app-lifecycle#ios
 
 [Register(nameof(AppDelegate))]
-public class AppDelegate : MauiUIApplicationDelegate
+public class AppDelegate : VertiGISAppDelegate
 {
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
