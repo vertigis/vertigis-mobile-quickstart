8f8b67c9e6efb2a41efb8e3e573d72fbde732215
Felicity Rhone <frhone@latitudegeo.com>
2024-12-13 10:35:31 -0800

    Create new Maui App

---
+++ App1.Maui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace App1.Maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
