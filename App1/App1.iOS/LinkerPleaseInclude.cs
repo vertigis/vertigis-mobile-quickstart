d4f038f821d046c6ae3695e741083a8e68bac943
Felicity Rhone <62772237+Felicity-R@users.noreply.github.com>
2025-02-13 13:27:27 -0800

    Maui conversion - Part II (#11)
    * Run upgrade tool on UWP project
    
    * Run upgrade tool on App1
    
    * Update Esri packages
    
    * Temporarily disable ios/android support
    
    * Switch to local Mobile package
    
    * Match maui version to Mobile's
    
    * Update XF reference
    
    * Fix some build errors
    
    * Add UseStudioMobile to MauiProgram
    
    * Fix resource paths
    
    * Another mobile package update
    
    * Migrate resource stuff from app.xaml.original
    
    * Remove (commented) reference to resources file that isn't supposed to exist anymore
    
    * Update app.xaml
    
    * Another Mobile package update
    
    * Run migration tool on Android project. Update nuget packages.
    
    * Update Android fileprovider - fixes 1st runtime error
    
    * mobile pkg update
    
    * UseStudioMobile
    
    * Migrate MainActivity code for Android
    
    * Include layout and other resources
    
    * Fix theme issue
    
    * Update styles file
    
    * Update mobile package
    
    * package update
    
    * Run upgrade tool
    
    * Update packages
    
    * Update mobile package
    
    * provisioning...
    
    * Fix sln configuration
    
    * Remove xamarin.forms reference
    
    * UseStudioMobile
    
    * AppDelegate subclass
    
    * More updates...
    
    * Fix MauiProgram
    
    * Update Mobile test package version
    
    * Clean up usings
    
    * Update AppDelegate
    
    * Undo testing changes
    
    * Update package
    
    * Undo testing change to layout-small
    
    * Update nuget testing version
    
    * Undo commented code
    
    * Correct package vesrion
    
    * Fix missing usings
    
    * Real package version update
    
    * Create new Maui App
    
    * Remove tizen and maccatalyst platforms
    
    * Copy split projects' stuff into single project
    
    * Remove shared project files - leaving only single-project behind
    
    * Remove Maui suffix from project name
    
    * Remove .maui suffix from root namespace
    
    * Add back XmlNamespaces file lost in conversion
    
    * fix some code warnings
    
    * fix warning about chaining usings in MauiProgram (shouldn't be necessary but ok)
    
    * Remove .Maui suffix from project folder
    
    * Update ci.yml
    
    * Update ci.yml
    
    * Update packages

--- App1/App1.iOS/LinkerPleaseInclude.cs
+++
@@ -1,9 +0,0 @@
-﻿//using Esri.ArcGISRuntime.Xamarin.Forms.Platform.iOS;
-
-//namespace App1.iOS
-//{
-//    // Get around what seems to be a linker issue, allowing custom MapViewRenderer to be used.
-//    class LinkerPleaseInclude : MapViewRenderer
-//    {
-//    }
-//}
---
+++ App1/MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using VertiGIS.Mobile.Platform;
+using Esri.ArcGISRuntime.Maui;
+using Esri.ArcGISRuntime.Toolkit.Maui;
+using CommunityToolkit.Maui;
+
+namespace App1
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseStudioMobile()
+                .UseArcGISRuntime()
+                .UseArcGISToolkit()
+                .UseMauiCommunityToolkit()
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+            return builder.Build();
+        }
+    }
+}
